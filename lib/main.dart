5768cd4743313ab76f7bba16bdc3611fa31c4101
adnanjpg <adnanfahedhr@gmail.com>
2023-07-30 17:19:33 +0300

    chore: update fb creds

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:afad_app/screens/start_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     const MaterialApp(
